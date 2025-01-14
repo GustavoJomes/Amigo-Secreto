d42b37d9cde796cd6abc42b8358f916a1af0ce61
GustavoJomes <protol.lapp@gmail.com>
2025-01-14 20:21:40 -0300

    Corrigir erros

--- js/app.js
+++ js/app.js
@@ -2,11 +2,24 @@ let listaNomes = []
 let listaSorteioOriginal = document.getElementById('lista-sorteio').value;
 let amigosIncluidos = document.getElementById('lista-amigos').value;
 function adicionar() {
-    listaNomes.push(document.getElementById('nome-amigo').value);
-    document.getElementById('lista-amigos').innerHTML = listaNomes;
-}
+    if (listaNomes.includes(document.getElementById('nome-amigo').value)) {
+        alert('Este nome já foi adicionado ao sorteio, informe outro!');
+    } else {
+        if (document.getElementById('nome-amigo').value == '') {
+            alert('Por favor preencha um nome no campo!');
+        } else{
+            listaNomes.push(document.getElementById('nome-amigo').value);
+            document.getElementById('lista-amigos').innerHTML = listaNomes;
+            document.getElementById('nome-amigo').value = '';
+        }
+        }
+    }
+    
 function sortear() {
-    embaralhar(listaNomes);
+    if (listaNomes.length < 3) {
+        alert('Adicione pelo menos 3 amigos');
+    } else {
+        embaralhar(listaNomes);
     document.getElementById('lista-sorteio').innerHTML = ''
     let i = 0
     let sorteio = document.getElementById('lista-sorteio');
@@ -17,7 +30,10 @@ function sortear() {
             sorteio.innerHTML = sorteio.innerHTML + listaNomes[i] + ' --> ' + listaNomes[i + 1] + ' <br>';
         }
         i++
+        
     }
+    }
+    
 }
 function embaralhar(array) {
     array.sort(() => Math.random() - 0.5);
@@ -31,3 +47,4 @@ function embaralhar(array) {
     document.getElementById('lista-sorteio').innerHTML = listaSorteioOriginal;
     document.getElementById('nome-amigo').value = '';
   }
+
